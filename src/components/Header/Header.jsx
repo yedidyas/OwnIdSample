63b6393ec7ab4e627c27418c690e621ddbe4610d
NivNavick <nnavick@gmail.com>
2022-01-17 21:54:51 +0200

    build

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,11 +6,11 @@ function Header() {
     return (
         <div className="header">
             <div className="logo">
-                <img width="80px"
+                <img alt="logo" width="80px"
                      src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png"/>
             </div>
             <div className="links">
-                <a href="" className="header-nav-a">Niv Navick</a>
+                <a href="www.nivnavick.com" className="header-nav-a">Niv Navick</a>
             </div>
         </div>
     );
--- src/components/OwnIdWidget/OwnIdWidget.jsx
+++ src/components/OwnIdWidget/OwnIdWidget.jsx
@@ -3,14 +3,10 @@ import {ownIdWidget} from "../../services/ownIdWidgetGenerator";
 
 function OwnIdWidget({type, container}) {
     useEffect(() => {
-        injectScript();
-    }, []);
-
-    function injectScript() {
         ownIdWidget.generateWidgetByType(type, container)
             .then((message) => console.log(message))
             .catch(err => console.error(err));
-    }
+    }, []);
 
     return (
         <div id={container}/>
